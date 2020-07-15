7949b9e50d0c3d00747ab24fa32042acb739a448
Pooja <67447083+psha85@users.noreply.github.com>
2020-07-14 17:57:36 -0700

    Delete WatchAndCodePracticalJavascript - v1.js

--- WatchAndCodePracticalJavascript - v1.js
+++
@@ -1,8 +0,0 @@
-var todos = ['item1', 'item2', 'item3']
-console.log('todos')
-console.log('My Todos:', todos)
-todos.push('item 4')
-todos[0]
-todos[1]
-todos[0] = 'item 1 updated'
-todos.splice(0, 1)
